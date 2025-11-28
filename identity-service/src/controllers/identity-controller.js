5ed7c51a39737511e1af753692241dbb43134997
caotrinhthinh <caotrinhthinh@gmail.com>
2025-11-28 16:45:16 +0700

    Add generateToken and registerUser controller

---
+++ identity-service/src/controllers/identity-controller.js
@@ -0,0 +1,51 @@
+import User from '../models/user-model.js';
+import RefreshToken from '../models/refresh-token-model.js';
+import logger from '../utils/logger.js';
+import { validateLogin, validateRegistration } from '../utils/validation.js';
+import generateTokens from '../utils/generateTokens.js';
+
+const registerUser = async (req, res) => {
+    logger.info('Registering new user: %s', req.body.username);
+    try {
+        // validate schema
+        const { error } = validateRegistration(req.body);
+        if (error) {
+            logger.warn('Validation error: %s', error.details[0].message);
+            return res.status(400).json({ success: false, message: error.details[0].message });
+        }
+
+        const { email, username, password } = req.body;
+
+        // check if user already exists
+        let user = await User.findOne({ $or: [{ email }, { username }] });
+
+        if (user) {
+            logger.warn('User already exists with email: %s or username: %s', email, username);
+            return res.status(409).json({ success: false, message: 'User already exists' });
+        }
+
+        user = new User({ email, username, password });
+        await user.save();
+
+        logger.info('User registered successfully: %s', username);
+
+        const { accessToken, refreshToken } = await generateTokens(user);
+
+        return res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            data: {
+                accessToken,
+                refreshToken,
+            },
+        });
+    } catch (error) {
+        logger.error('Error registering user: %o', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+};
+const loginUser = async (req, res) => {};
+const refreshToken = async (req, res) => {};
+const logoutUser = async (req, res) => {};
+
+export { registerUser, loginUser, refreshToken };
---
+++ identity-service/src/utils/validation.js
@@ -0,0 +1,21 @@
+import joi from 'joi';
+import { use } from 'react';
+
+const validateRegistration = (data) => {
+    const schema = joi.object({
+        username: joi.string().alphanum().min(3).max(30).required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(6).required(),
+    });
+    return schema.validate(data);
+};
+
+const validateLogin = (data) => {
+    const schema = joi.object({
+        username: joi.string().required(),
+        password: joi.string().required(),
+    });
+    return schema.validate(data);
+};
+
+export { validateRegistration, validateLogin };
